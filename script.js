c98b1e53454075ddb40984e45bb69a35a317b9e4
Kotbegemotl <72646013+Kotbegemotl@users.noreply.github.com>
2021-04-14 10:11:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// for (let i = 0; i <= 9; i++){
+//     console.log(i);
+// }
+
+// let arr = [];
+// for (let i = 0; i < 10; i++){
+//         arr = i;
+//     console.log(arr);
+// }
+
+// let arr = [];
+// for (let i = 0; i < 10; i++){
+//     arr.push(i);
+// }
+// console.log(arr);
+
+// for(let i = 0; i < 3; i++){
+//     let a = 0;
+//     if(i == 2){
+//         i = 0;
+//     }
+//     console.log(a,i);
+// }
+
+let arr = [];
+
+for (let i = 0; i < 4; i++) {
+    arr[i] = [];
+    for (let j = 0; j < 4; j++) {
+            arr[j] = i + j;
+    }
+    console.log(arr);
+}
